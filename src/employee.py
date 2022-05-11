59bc123ba63c6f7adb0a9a75443b879df442a323
mlokos <mlokos.github@gmail.com>
2022-05-11 15:44:29 +0200

    add black fromatting

--- src/employee.py
+++ src/employee.py
@@ -4,11 +4,16 @@ from src.utilities import SavAdapter
 from pyreadstat._readstat_parser import metadata_container
 
 
-def get_job_title_generalized(job_title: float, generalization: int, specialization: Optional[int]) -> float:
+def get_job_title_generalized(
+    job_title: float, generalization: int, specialization: Optional[int]
+) -> float:
     str_value = str(job_title)
 
-    jobs_range = generalization + len(str(specialization)) if specialization else generalization
+    jobs_range = (
+        generalization + len(str(specialization)) if specialization else generalization
+    )
     return float(str_value[:jobs_range])
+
 
 def get_competencies_list() -> List[str]:
     competencies_keys = ["k0" + str(i) for i in range(1, 10)]
@@ -16,29 +21,35 @@ def get_competencies_list() -> List[str]:
 
     return competencies_keys
 
-def get_job_title_to_competencies(df: DataFrame, df_meta: metadata_container, generalization: int, specialization: Optional[int]) -> DataFrame:
+
+def get_job_title_to_competencies(
+    df: DataFrame,
+    df_meta: metadata_container,
+    generalization: int,
+    specialization: Optional[int],
+) -> DataFrame:
     df = df.dropna(subset=["e10_isco"], axis=0)
 
     if specialization:
         employees_with_specialization_mask = [
             str(x).startswith(str(specialization)) for x in df["e10_isco"]
-            ]
+        ]
         df = df[employees_with_specialization_mask]
 
     df = df.assign(
-        e10_isco_generalized = df["e10_isco"].apply(
+        e10_isco_generalized=df["e10_isco"].apply(
             lambda x: get_job_title_generalized(x, generalization, specialization)
-            )
         )
+    )
     df = df.groupby("e10_isco_generalized").mean()
     df = df.reset_index()
 
-    sad = SavAdapter(metadata = df_meta)
+    sad = SavAdapter(metadata=df_meta)
     df = df.assign(
-        e10_isco_name = df["e10_isco_generalized"].apply(
+        e10_isco_name=df["e10_isco_generalized"].apply(
             lambda x: sad.get_value_name("e10_isco", x)
-            )
         )
+    )
 
     employees_to_competencies = df[["e10_isco_name", *get_competencies_list()]]
     employees_to_competencies = employees_to_competencies.set_index("e10_isco_name")
--- src/utilities.py
+++ src/utilities.py
@@ -8,80 +8,90 @@ from pyreadstat._readstat_parser import metadata_container
 
 
 def load_sav_data() -> Tuple[DataFrame, DataFrame]:
-    path_employee_df = Path('bin/employee_sav_df.pkl')
-    path_employee_meta = Path('bin/employee_sav_meta.pkl')
-    path_boss_df = Path('bin/boss_sav_df.pkl')
-    path_boss_meta = Path('bin/boss_sav_meta.pkl')
+    path_employee_df = Path("bin/employee_sav_df.pkl")
+    path_employee_meta = Path("bin/employee_sav_meta.pkl")
+    path_boss_df = Path("bin/boss_sav_df.pkl")
+    path_boss_meta = Path("bin/boss_sav_meta.pkl")
 
     employee_sav_df = None
     employee_sav_meta = None
     boss_sav_df = None
     boss_sav_meta = None
 
-    bin_path = Path('bin')
+    bin_path = Path("bin")
     if not bin_path.exists():
         bin_path.mkdir(exist_ok=False)
 
-    if all([
-        path_employee_df.is_file(),
-        path_employee_meta.is_file(),
-        path_boss_df.is_file(),
-        path_boss_meta.is_file()
-    ]):
-        with open(path_employee_df, 'rb') as file:
+    if all(
+        [
+            path_employee_df.is_file(),
+            path_employee_meta.is_file(),
+            path_boss_df.is_file(),
+            path_boss_meta.is_file(),
+        ]
+    ):
+        with open(path_employee_df, "rb") as file:
             employee_sav_df = pickle.load(file)
-        with open(path_employee_meta, 'rb') as file:
+        with open(path_employee_meta, "rb") as file:
             employee_sav_meta = pickle.load(file)
-        with open(path_boss_df, 'rb') as file:
+        with open(path_boss_df, "rb") as file:
             boss_sav_df = pickle.load(file)
-        with open(path_boss_meta, 'rb') as file:
+        with open(path_boss_meta, "rb") as file:
             boss_sav_meta = pickle.load(file)
     else:
-        employee_sav_df, employee_sav_meta = read_sav('datasets/Baza_danych_z_badania_ludnoci_BKL_edycja_2019.sav')
-        boss_sav_df, boss_sav_meta = read_sav('datasets/Baza_danych_z_badania_pracodawcw_BKL_edycja_2019.sav')
+        employee_sav_df, employee_sav_meta = read_sav(
+            "datasets/Baza_danych_z_badania_ludnoci_BKL_edycja_2019.sav"
+        )
+        boss_sav_df, boss_sav_meta = read_sav(
+            "datasets/Baza_danych_z_badania_pracodawcw_BKL_edycja_2019.sav"
+        )
 
-        with open(path_employee_df, 'wb') as file:
+        with open(path_employee_df, "wb") as file:
             pickle.dump(employee_sav_df, file)
-        with open(path_employee_meta, 'wb') as file:
+        with open(path_employee_meta, "wb") as file:
             pickle.dump(employee_sav_meta, file)
-        with open(path_boss_df, 'wb') as file:
+        with open(path_boss_df, "wb") as file:
             pickle.dump(boss_sav_df, file)
-        with open(path_boss_meta, 'wb') as file:
+        with open(path_boss_meta, "wb") as file:
             pickle.dump(boss_sav_meta, file)
 
     return employee_sav_df, employee_sav_meta, boss_sav_df, boss_sav_meta
-    
+
 
 def load_spss_data() -> Tuple[DataFrame, DataFrame]:
-    path_employee = Path('bin/employee_spss_df.pkl')
-    path_boss = Path('bin/boss_spss_df.pkl')
+    path_employee = Path("bin/employee_spss_df.pkl")
+    path_boss = Path("bin/boss_spss_df.pkl")
 
     employee_spss_df = None
     boss_spss_df = None
-    
-    bin_path = Path('bin')
+
+    bin_path = Path("bin")
     if not bin_path.exists():
         bin_path.mkdir(exist_ok=False)
-        
+
     if all([path_employee.is_file(), path_boss.is_file()]):
-        with open(path_employee, 'rb') as file:
+        with open(path_employee, "rb") as file:
             employee_spss_df = pickle.load(file)
-        with open(path_boss, 'rb') as file:
+        with open(path_boss, "rb") as file:
             boss_spss_df = pickle.load(file)
     else:
-        employee_spss_df = read_spss('datasets/Baza_danych_z_badania_ludnoci_BKL_edycja_2019.sav')
-        boss_spss_df = read_spss('datasets/Baza_danych_z_badania_pracodawcw_BKL_edycja_2019.sav')
+        employee_spss_df = read_spss(
+            "datasets/Baza_danych_z_badania_ludnoci_BKL_edycja_2019.sav"
+        )
+        boss_spss_df = read_spss(
+            "datasets/Baza_danych_z_badania_pracodawcw_BKL_edycja_2019.sav"
+        )
 
-        with open(path_employee, 'wb') as file:
+        with open(path_employee, "wb") as file:
             pickle.dump(employee_spss_df, file)
-        with open(path_boss, 'wb') as file:
+        with open(path_boss, "wb") as file:
             pickle.dump(boss_spss_df, file)
 
     return employee_spss_df, boss_spss_df
 
 
 @dataclass
-class SavAdapter():
+class SavAdapter:
     metadata: metadata_container
 
     def get_column_values_dict(self, column_name: str) -> dict:
@@ -90,7 +100,9 @@ class SavAdapter():
 
         return label_dict
 
-    def get_value_name(self, column_name: str, value: Union[str, float]) -> Union[str, float]:
+    def get_value_name(
+        self, column_name: str, value: Union[str, float]
+    ) -> Union[str, float]:
         label_dict = self.get_column_values_dict(column_name)
 
         return label_dict[value]
